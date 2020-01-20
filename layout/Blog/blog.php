6a8f5ccd8146b788f45cb975298a6f34d2090ff6
kikifauziaa <fauziahrizky22@gmail.com>
2020-01-20 09:53:27 +0700

    web 7

---
+++ layout/Blog/blog.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Rumah Murah Berkualitas Jawara Corp | Explore Your System with Us</title>
+    <link rel="shortcut icon" href="./gambarIndex/icon/JAWARA-icon-small.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="blog.css"> 
+    <link rel="stylesheet" href="blogcss/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="blogcss/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    -->
+</head>
+<body>
+    <nav class="navbar navbar-expand-xl navbar-light">
+        <div class="navbar-brand">
+            <a href=".index1.html" class="navbar-logo"><img src="https://jawaracorpo.com/assets/images/logo.png" alt="Explore Your System with Us" title="Explore Your System with Us"></a> 
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger-icon"></div>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    ON JOSS
+                    </a>
+                    <div class="dropdown">
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">WJOSS Marketplace</a>
+                        <a class="dropdown-item" href="#">Team JOSS</a>
+                        <a class="dropdown-item" href="#">Force Link</a>
+                        <a class="dropdown-item" href=""> User JOSS</a>
+                        <a class="dropdown-item" href="#">Rumah Murah Berkualitas Jawara Corp</a>
+                        <a class="dropdown-item" href="#">cakroden.com</a>
+                        <a class="dropdown-item" href="#">wimapu.com</a>
+                        <a class="dropdown-item" href="">vilbupin.com</a>
+                        <a class="dropdown-item" href="#">Batu Top View</a>
+                        <a class="dropdown-item" href="#">Jawara Land</a>
+                        <a class="dropdown-item" href="#">Jawara Boulevard</a>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><span class="mbri-info"></span>Tentang</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontak</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Widget</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="isian">
+        <div class="judul">
+            <h1>Blog Rumah Murah Berkualitas Jawara Corp</h1>
+            <h5>ini tulisan berjalan</h5>
+        </div>
+        <div class="background-gelap">
+            <form class="form-inline">
+                <h6>Isikan kata yang data cari</h6> 
+                <label class="form-control" type="search" placeholder="Search" aria-label=""></label>
+            </form>
+            <div class="input-group">
+                <h6>Pilih Kategori</h6>
+                <select class="col-sm-4" id="inputGroupSelect04">
+                    <option selected>Pilih Kategori</option>
+                    <option value="1">General</option>
+                    <option value="2">Rumah Murah</option>
+                    <option value="3">Rumah dan Keluarga</option>
+                    <option value="4">Desain Eksterior</option>
+                    <option value="5">Information</option>
+                    <option value="6">Pendidikan</option>
+                    <option value="7">Sosial Budaya</option>
+                    <option value="8">Agama</option>
+                    <option value="9">Perumahan</option>
+                    <option value="10">Progres</option>
+                    <option value="11">Sepak Bola</option>
+                    <option value="12">Tips dan Trik</option>
+                    <option value="13">Seputar Olahraga</option>
+                    <option value="="14>Ragam Daftar</option>
+                </select>
+                <div class="input-append">   
+                        <button class="btn btn-secondary" type="button" style="width: 50px;">
+                            <i class="fas fa-search" style="color: gray;"></i>
+                        </button>
+                </div>
+            </div>
+            <div class='row'>
+
+
+            <div class='row'>
+<?php
+foreach($items as $item) {
+  echo "<div class='col-lg-2'>";
+      echo "<div class='item'>";
+        echo 'Anythin';
+      echo '</div>';
+  echo '</div>';
+  $i++;
+  if ($i % 3 == 0) {echo '</div><div class="row">';}
+}
+?>
+</div>
+
+
+                
+                
+
+            </div>
+        </div>
+            <nav aria-label="Page navigation example" >
+                <ul class="pagination" style="background-color: rgba(0, 0, 0, 0);">
+                  <li class="page-item" style="background-color: rgba(0, 0, 0, 0);">
+                    <a class="page-link" href="#" aria-label="Previous">
+                      <span aria-hidden="true">&laquo;</span>
+                    </a>
+                  </li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                      <span aria-hidden="true">&raquo;</span>
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+    </div>
+
+    <div class="map">
+        <div class="row">
+            <div class="col-3">
+                <strong style="color:#b7b5b5;">
+                    <span class="mbri-home mbr-iconfont"></span> &nbsp;Address</strong
+                    ><br>
+                <i class="fas fa-home" style="color: white;"></i>
+                <a style="font-size: medium; color: white; font-weight: bolder;">Address</a>
+                <a class="pt" style = "font-size: small; color: white; font-weight: bolder;">JawaraCorporation :</a><br>
+                <a class="alamat" style = "font-size: small; color: white;">Ruko Pesanggrahan Pratama Blok B-6
+                    Jl. Panglima Sudirman - Karangploso - Malang.
+                    East Java.
+                    Indonesia.
+                    Kode Pos 65152</a><br><br><br>
+                <i class="fas fa-clock-o" style="color: white;"></i>
+                <a style="font-size: medium; color: white; font-weight: bolder;">Opening hour</a><br><br>
+                <a class="alamat" style = "font-size: small; color: white;">08:30:00 - 16:30:00</a>
+            </div>
+            <div class="col-3">
+                <i class="fas fa-sticky-note" style="color: white;"></i>
+                <a style="font-size: medium; color: white; font-weight: bolder;">Link</a>
+                <li>
+                    <ul>
+                        <a>+6282336483333</a>
+                    </ul>
+
+                </li>
+            </div>
+            <div class="col-6">
+               <i class="fas fa-" style="color: white;"></i>
+                <a style="font-size: medium; color: white; font-weight: bolder;">Google Map : Office</a>
+
+            </div>
+        </div>
+    </div>
+
+    <footer style="align-items: center;">
+        <div class="judul-kategori">Kategori
+            <!-- <p class="mbr-section-subtitle" style="text-align: center;"></p> -->
+        </div><br><br>
+
+        <p class="tulisan-footer">        	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Ulasan-Produk/" title="Ulasan Produk">
+                                      
+						Ulasan Produk <small style="color:#d0d0d0;"> <strong>68</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Tips-dan-Trik/" title="Tips dan Trik">
+                                      
+						Tips dan Trik <small style="color:#d0d0d0;"> <strong>36</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Teknologi-dan-Dunia-Digital/" title="Teknologi dan Dunia Digital">
+                                      
+						Teknologi dan Dunia Digital <small style="color:#d0d0d0;"> <strong>5</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Sosial-Budaya/" title="Sosial Budaya">
+                                      
+						Sosial Budaya <small style="color:#d0d0d0;"> <strong>5</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Seputar-Olahraga/" title="Seputar Olahraga">
+                                      
+						Seputar Olahraga <small style="color:#d0d0d0;"> <strong>3</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Sepak-Bola/" title="Sepak Bola">
+                                      
+						Sepak Bola <small style="color:#d0d0d0;"> <strong>12</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Rumah-Murah/" title="Rumah Murah">
+                                      
+						Rumah Murah <small style="color:#d0d0d0;"> <strong>5</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Rumah-dan-Keluarga/" title="Rumah dan Keluarga">
+                                      
+						Rumah dan Keluarga <small style="color:#d0d0d0;"> <strong>5</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Ragam-Daftar/" title="Ragam Daftar">
+                                      
+						Ragam Daftar <small style="color:#d0d0d0;"> <strong>2</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Progres/" title="Progres">
+                                      
+						Progres <small style="color:#d0d0d0;"> <strong>46</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Perumahan/" title="Perumahan">
+                                      
+						Perumahan <small style="color:#d0d0d0;"> <strong>4</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Pendidikan/" title="Pendidikan">
+                                      
+						Pendidikan <small style="color:#d0d0d0;"> <strong>4</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Jawara-Cyber-Partner/" title="Jawara Cyber Partner">
+                                      
+						Jawara Cyber Partner <small style="color:#d0d0d0;"> <strong>3</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Inspirasi-dan-Motivasi/" title="Inspirasi dan Motivasi">
+                                      
+						Inspirasi dan Motivasi <small style="color:#d0d0d0;"> <strong>1</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Information/" title="Information">
+                                      
+						Information <small style="color:#d0d0d0;"> <strong>16</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/General/" title="General">
+                                      
+						General <small style="color:#d0d0d0;"> <strong>5</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Feng-Shui/" title="Feng Shui">
+                                      
+						Feng Shui <small style="color:#d0d0d0;"> <strong>5</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Economic-Portal/" title="Economic Portal">
+                                      
+						Economic Portal <small style="color:#d0d0d0;"> <strong>3</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Desain-Rumah/" title="Desain Rumah">
+                                      
+						Desain Rumah <small style="color:#d0d0d0;"> <strong>13</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Desain-Interior/" title="Desain Interior">
+                                      
+						Desain Interior <small style="color:#d0d0d0;"> <strong>102</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Desain-Eksterior/" title="Desain Eksterior">
+                                      
+						Desain Eksterior <small style="color:#d0d0d0;"> <strong>5</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Berita-Jawara/" title="Berita Jawara">
+                                      
+						Berita Jawara <small style="color:#d0d0d0;"> <strong>13</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Artikel-Rumah/" title="Artikel Rumah">
+                                      
+						Artikel Rumah <small style="color:#d0d0d0;"> <strong>158</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori/Agama/" title="Agama">
+                                      
+						Agama <small style="color:#d0d0d0;"> <strong>4</strong> artikel</small>
+                    
+                    </a>
+	 
+                    <a style="padding: 0 10px 0 0;" href="https://jawaracorpo.com/cari-artikel/kategori//" title="">
+                                      
+						 <small style="color:#d0d0d0;"></small>
+                    
+                    </a><br><br>
+        </p><br><br>
+        <!-- <a class="tulisan-copyright">Copyright© Jawara Corporation September - 2017</a>
+        <div class="navbar-brand">
+            <a href="..index1.html" class="navbar-logo"><img src="https://jawaracorpo.com/assets/images/logo.png" alt="Explore Your System with Us" title="Explore Your System with Us"></a> 
+        </div> -->
+        <div class="judul-service">Terms of Service
+            <!-- <p style="text-align: center;"></p> -->
+        </div><br>
+        <div class="copyright">
+            <a style="margin-left: 370px;">Copyright© Jawara Corporation September - 2017</a>
+        </div>
+        <div class="navbar-brand">
+            <a href="..index1.html" class="navbar-logo">
+                <img src="https://jawaracorpo.com/assets/images/logo.png" alt="Explore Your System with Us" title="Explore Your System with Us"></a> 
+        </div>
+    </footer>
+
+
+    
+    
+    
+    
+</body>
+</html>
